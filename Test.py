627732f438284c74396d847d607583f7e04a9b09
PHAT V <34657152+vochiphat10@users.noreply.github.com>
2026-04-26 15:03:24 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -602,5 +602,6 @@ def update_dashboard(year_values, region_values, gender_values, education_values
 #run dashapp on web browser
 
 server = app.server
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
